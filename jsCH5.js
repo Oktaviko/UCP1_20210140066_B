588d63db3a611bb502ea11a84611ea25d9cacb66
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-05-21 13:03:27 +0700

    Create NEW HTML

---
+++ jsCH5.js
@@ -0,0 +1,9 @@
+video.addEventListener("click", function video(){
+    alert("ini vidio aku chuaksss");
+});
+audio.addEventListener("click", function audio(){
+    alert("ini audio aku chuaksss");
+});
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar aku chuaksss");
+});
---
+++ script.js
@@ -0,0 +1,16 @@
+//Date() untuk menampilkan pesan pada browser
+Date()
+
+//Mendapatkan tanggal hari ini
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = tanggal ;
+
+//untuk menampilkan alert tampil()
+function tampil(){
+    alert("Halaman ini ditulis dengan Oktaviko Rizki Pratama")
+}
+
+//untuk menampilkan alert show()
+function show(){
+    alert("Ini adalah pop up untuk tulisan perkenalan")
+}
